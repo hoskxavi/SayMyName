697318be03ce74f34660d27046c9be7964ac704c
hoskxavi <hoskmunch@gmail.com>
2022-08-30 08:37:19 -0600

    Alter code for Emily and Joe

--- SayMyName/Program.vb
+++ SayMyName/Program.vb
@@ -15,8 +15,19 @@ Module Program
 
         Console.WriteLine("Please enter your name:")
         userInput = Console.ReadLine()
-        Console.Write("Your name is ")
-        Console.WriteLine(userInput)
+
+        If userInput = "Emily" Or userInput = "EMILY" Or userInput = "emily" Then
+            Console.WriteLine("Hello Emily")
+        ElseIf userInput = "Joe" Or userInput = "JOE" Or userInput = "joe" Then
+            Console.WriteLine("Hello Joe")
+        ElseIf userInput = "Xavier" Or userInput = "XAVIER" Or userInput = "xavier" Then
+            Console.WriteLine("The creator has spoken...")
+        ElseIf userInput = "Dave" Or userInput = "DAVE" Or userInput = "dave" Then
+            Console.WriteLine("I'm sorry Dave, but I'm afraid I can't do that...")
+        Else
+            Console.WriteLine("YOU ARE NOT AUTHORIZED")
+        End If
+
 
     End Sub
 End Module
